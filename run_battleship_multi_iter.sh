8d7f931447cd33a1485f1eed723fa2bd550f64ff
劉達融 <givebirthday@login.speech>
2020-08-20 10:49:04 +0800

    add uns match different stage config, add battleship run script for multistage training

---
+++ run_battleship_multi_iter.sh
@@ -0,0 +1,29 @@
+# !/bin/bash
+
+### Experimental Setting
+. ./config_battleship.sh
+
+### overall prefix
+gan_config=$(readlink -m $1)
+overall_prefix=$2
+
+if [ $bnd_type == orc ]; then
+  total_iter=1
+else
+  total_iter=3
+fi
+
+### Preprocess TIMIT
+# bash preprocess.sh
+
+### Training Process
+for iteration in $(seq 1 $total_iter); do
+  ### train GAN model
+  hrun -G -c $jobs -m 32 bash -c '. ./cmd.sh; . ./path.sh; . ./config_battleship.sh; cd src; bash train_GAN.sh '$iteration' '$gan_config'_'$iteration'.yaml '$overall_prefix || exit 1
+
+  ### wfst decoder
+  hrun -c $jobs -m 64   bash -c '. ./cmd.sh; . ./path.sh; . ./config_battleship.sh; cd src; bash train_wfst.sh '$iteration' '$overall_prefix || exit 1
+
+  ### train HMM and get new boundaries
+  hrun -c $jobs -m 64   bash -c '. ./cmd.sh; . ./path.sh; . ./config_battleship.sh; cd src; bash train_HMM.sh '$iteration' '$overall_prefix || exit 1
+done
