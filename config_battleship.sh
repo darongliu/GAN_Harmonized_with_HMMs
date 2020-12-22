3e3e12e7f482e7bd8b9a032cf3661d456f49867c
劉達融 <givebirthday@login.speech>
2020-12-22 21:14:30 +0800

    minored

--- config_battleship.sh
+++ config_battleship.sh
@@ -3,13 +3,15 @@
 #Path
 export ROOT_DIR=/home/givebirthday/GAN_Harmonized_with_HMMs #abs. path of this github repository
 export TIMIT_DIR=/groups/givebirthday/GAN_Harmonized_with_HMMs/timit #abs. path of your timit dataset
-export DATA_PATH=/groups/givebirthday/GAN_Harmonized_with_HMMs/data
+export DATA_PATH=/groups/givebirthday/GAN_Harmonized_with_HMMs/nonmatch-data
+#export DATA_PATH=/groups/givebirthday/GAN_Harmonized_with_HMMs/data
 
 #Boundaries type: orc / uns
-export bnd_type=orc
+export bnd_type=uns
 
 #Setting: match / nonmatch
-export setting=match
+export setting=nonmatch
+#export setting=match
 
 #Number of jobs
-export jobs=8
+export jobs=16
